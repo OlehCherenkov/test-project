d8bcdff30341721bdc8ee3fe93e253055cd27e81
olehcherenkov <samsunggio570@gmail.com>
2021-12-22 16:19:34 +0200

    Minor fixes, added views for posts

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
-    public function index() {
-        return view('welcome');
+    public function index()
+    {
+        $posts = Post::orderByDesc('created_at')->limit(3)->get();
+        return view('welcome', compact('posts'));
     }
 }
--- app/Http/Middleware/GoogleRecaptcha.php
+++ app/Http/Middleware/GoogleRecaptcha.php
@@ -16,9 +16,6 @@ class GoogleRecaptcha
      */
     public function handle(Request $request, Closure $next)
     {
-        if(!$request->has('r')) {
-            abort(403);
-        }
         return $next($request);
     }
 }
